efab866009edf6f467d628f06a44dc74ec938fc6
Jack1eAW <alcheb2014@gmail.com>
2022-10-23 08:35:35 +0300

    Initial Commit

---
+++ OTUS-GPB-TableView-HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTUS-GPB-TableView-HW
+//
+//  Created by Aleksandr Chebotarev on 10/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
